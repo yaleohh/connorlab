9426830b999d0510c706d3e7892cbfe7875677ac
yohh <yohh@cisco.com>
2017-05-05 11:59:04 -0400

    Initializing Folder

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+// -------------------------  MAIN  -------------------------
+// open mobile menu
+$('.js-toggle-menu').click(function(e){
+  e.preventDefault();
+  $('.mobile-header-nav').slideToggle();
+  $(this).toggleClass('open');
+});
